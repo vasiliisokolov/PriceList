e4675255016841ede0ce38f80e28a3d0f7402fef
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-12 20:34:52 +0300

    Добавьте файлы проекта.

---
+++ PriceList.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int totalCost;
+    std::vector<float> prices { 2.5, 4.25, 3.0, 10.0 };
+    std::vector<int> items { 1, 1, 0, 3 };
+    std::cout << "Total cost of purchases:" << totalCost << std::endl;
+}
