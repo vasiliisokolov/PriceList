d2a12097e9a17f6703b1e15f49211d7b12a627f4
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-12 20:47:34 +0300

    done

--- PriceList.cpp
+++ PriceList.cpp
@@ -3,8 +3,15 @@
 
 int main()
 {
-    int totalCost;
+    float totalCost = 0;
     std::vector<float> prices { 2.5, 4.25, 3.0, 10.0 };
     std::vector<int> items { 1, 1, 0, 3 };
+
+    for (int i = 0; i < items.size(); i++)
+    {
+        //std::cout << prices[(int)items[i]] << std::endl;
+        totalCost += prices[(int)items[i]];
+    }
+
     std::cout << "Total cost of purchases:" << totalCost << std::endl;
 }
